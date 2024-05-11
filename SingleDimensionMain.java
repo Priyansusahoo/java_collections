2336bc085f45691efe9f7f034c8dd4f0d0cc7b89
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-05-11 16:42:11 +0530

    added: SingleDimensionMain.java

---
+++ SingleDimensionMain.java
@@ -0,0 +1,11 @@
+/**
+ * @author Priyansu Sahoo
+ * @created 11-05-2024 - 03:40 pm
+ */
+public class SingleDimensionMain {
+    public static void main(String[] args) {
+        int[] arr = new int[1000];
+
+        System.out.println(arr.getClass().getName());
+    }
+}
